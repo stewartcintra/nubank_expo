5100e23f3956847fa3b8270fa89e1f595995ffee
Stewart Cintra <stewartcintra@hotmail.com>
2019-12-27 12:24:59 -0300

    finalizado nubank tela inicial

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,51 @@
 import React from 'react';
-import QRCode from 'react-native-qrcode';
+//import QRCode from 'react-native-qrcode';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Code } from './styles';
+import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles';
 
-export default function Menu() {
+export default function Menu({ translateY }) {
   return (
-    <Container>
+    <Container style={{
+      opacity: translateY.interpolate({
+        inputRange: [0, 150],
+        outputRange: [0, 1],
+      }),
+    }}>
         <Code>
-            <QRCode
-                value="http://rocketseat.com.br"
-                size={80}
-                bgColor="#fff"
-                fbColor="#8b10ae"
-            />
+          {/* <QRCode
+              value="http://rocketseat.com.br"
+              size={80}
+              fbColor="#fff"
+              bgColor="#8b10ae"
+          /> */}
         </Code>
+
+        <Nav>
+          <NavItem>
+            <Icon name="help-outline" size={20} color="#fff" />
+            <NavText>Me ajuda</NavText>
+          </NavItem>
+
+          <NavItem>
+            <Icon name="person-outline" size={20} color="#fff" />
+            <NavText>Perfil</NavText>
+          </NavItem>
+
+          <NavItem>
+            <Icon name="credit-card" size={20} color="#fff" />
+            <NavText>Configurar cartão</NavText>
+          </NavItem>
+
+          <NavItem>
+            <Icon name="smartphone" size={20} color="#fff" />
+            <NavText>Configurações do app</NavText>
+          </NavItem>
+        </Nav>
+
+        <SignOutButton onPress={() => {}}>
+          <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+        </SignOutButton>
     </Container>
   );
 }
